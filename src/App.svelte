830b0a78dbcfdc12b0b30ce1d7bd1f483c4e1ad4
Vladislav Bukatov <vasyayp@yandex.ru>
2021-04-05 00:46:50 +0400

    update telegram url

--- src/App.svelte
+++ src/App.svelte
@@ -1,93 +1,129 @@
 <script>
-  import List from "./List.svelte";
-  import Certificates from "./Certificates.svelte";
-  import Projects from "./Projects.svelte";
-  import Hand from "./Hand.svelte";
+  import List from './List.svelte'
+  import Certificates from './Certificates.svelte'
+  import Projects from './Projects.svelte'
+  import Hand from './Hand.svelte'
 
-  const avatar = "./static/avatar.jpg";
-  const ghCF = "./static/GrasshopperCodingFundamentalsCertificate.pdf";
-  const ghCF2 = "./static/GrasshopperCodingFundamentalsIICertificate.pdf";
-  const ghCF4 = "./static/GrasshopperIntrotoInterviewingCertificate.pdf";
+  const avatar = './static/avatar.jpg'
+  const ghCF = './static/GrasshopperCodingFundamentalsCertificate.pdf'
+  const ghCF2 = './static/GrasshopperCodingFundamentalsIICertificate.pdf'
+  const ghCF4 = './static/GrasshopperIntrotoInterviewingCertificate.pdf'
   const skills = [
-    "javascript(es6+), typescript",
-    "react",
-    "redux",
-    "vue.js",
-    "scss, styled-components, PostCSS",
-    "webpack, rollup",
-    "git",
-    "linux",
-  ];
+    'javascript(es6+), typescript',
+    'react',
+    'redux',
+    'vue.js',
+    'scss, styled-components, PostCSS',
+    'webpack, rollup',
+    'git',
+    'linux',
+  ]
 
   const social = [
-    { name: "github", link: "https://github.com/themetronome" },
-    { name: "telegram", link: "https://t.me/easymon3y5niper" },
-    { name: "vasyayp@yandex.ru", link: "mailto:vasyayp@yandex.ru" },
-  ];
+    { name: 'github', link: 'https://github.com/themetronome' },
+    { name: 'telegram', link: 'https://t.me/themetronome' },
+    { name: 'vasyayp@yandex.ru', link: 'mailto:vasyayp@yandex.ru' },
+  ]
 
   const courses = {
     fundamentals: [
       {
-        name: "js coding fundamentals",
+        name: 'js coding fundamentals',
         link: ghCF,
-        year: "2019",
-        company: "google",
+        year: '2019',
+        company: 'google',
       },
       {
-        name: "js coding fundamentals",
+        name: 'js coding fundamentals',
         link: ghCF2,
-        year: "2019",
-        company: "google",
+        year: '2019',
+        company: 'google',
       },
     ],
     other: [
       {
         name:
-          "intro to interviewing. at the end of this course i solved challenging google interview question.",
+          'intro to interviewing. at the end of this course i solved challenging google interview question.',
         link: ghCF4,
-        year: "2019",
-        company: "google",
+        year: '2019',
+        company: 'google',
       },
     ],
-  };
+  }
 
   const projects = [
     {
-      name: "todo",
-      link: "https://vue-sandbox-todo.now.sh/",
-      github: "https://github.com/themetronome/v-sandbox-todo",
-      desc: "simple vue todo",
-      tags: ["vue"],
+      name: 'todo',
+      link: 'https://vue-sandbox-todo.now.sh/',
+      github: 'https://github.com/themetronome/v-sandbox-todo',
+      desc: 'simple vue todo',
+      tags: ['vue'],
     },
     {
-      name: "challengify",
-      link: "https://challengify.now.sh/",
-      github: "https://github.com/themetronome/challengify",
-      desc: "30-day challange app",
-      tags: ["react"],
+      name: 'challengify',
+      link: 'https://challengify.now.sh/',
+      github: 'https://github.com/themetronome/challengify',
+      desc: '30-day challange app',
+      tags: ['react'],
     },
     {
-      name: "pizzahh",
-      link: "https://pizzahh.now.sh/",
-      github: "https://github.com/themetronome/pizzahh",
-      desc: "pizza restaurant site",
-      tags: ["react", "styled components"],
+      name: 'pizzahh',
+      link: 'https://pizzahh.now.sh/',
+      github: 'https://github.com/themetronome/pizzahh',
+      desc: 'pizza restaurant site',
+      tags: ['react', 'styled components'],
     },
     {
-      name: "sneakerbox",
-      link: "https://sneakerbox.now.sh/",
-      github: "https://github.com/themetronome/sneakerbox",
-      desc: "sneakershop",
-      tags: ["react", "react-router", "redux", "redux-thunk"],
+      name: 'sneakerbox',
+      link: 'https://sneakerbox.now.sh/',
+      github: 'https://github.com/themetronome/sneakerbox',
+      desc: 'sneakershop',
+      tags: ['react', 'react-router', 'redux', 'redux-thunk'],
     },
-  ];
+  ]
 </script>
+
+<container>
+  <div class="main">
+    <visually-hidden className="visually-hidden"> Vladislav Bukatov's website </visually-hidden>
+    <div class="centered">
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img class="avatar" src={avatar} width="150" height="150" alt="Vladislav Bukatov's photo" />
+      <Hand />
+    </div>
+
+    <div class="centered">
+      <h1>Vladislav Bukatov</h1>
+      <h2 class="position">frontend engineer</h2>
+    </div>
+    <block>
+      <h3 class="title">links</h3>
+      <List elements={social} />
+    </block>
+    <block>
+      <h3 class="title">skills</h3>
+      <ul>
+        {#each skills as skill}
+          <li class="skill-item">{skill}</li>
+        {/each}
+      </ul>
+    </block>
+    <block>
+      <h3 class="title">projects</h3>
+      <Projects {projects} />
+    </block>
+    <block>
+      <h3 class="title">certificates</h3>
+      <Certificates {courses} />
+    </block>
+  </div>
+</container>
 
 <style>
   :global(html) {
     background-color: rgba(242, 242, 230, 0.1);
     color: #242535;
-    font-family: "Helvetica Neue", sans-serif;
+    font-family: 'Helvetica Neue', sans-serif;
   }
   container {
     max-width: 900px;
@@ -97,7 +133,7 @@
     align-items: center;
     flex-direction: column;
     padding: 32px;
-    font-family: "Montserrat", sans-serif;
+    font-family: 'Montserrat', sans-serif;
     font-size: 16px;
     line-height: 24px;
   }
@@ -151,46 +187,3 @@
     }
   }
 </style>
-
-<container>
-  <div class="main">
-    <visually-hidden className="visually-hidden">
-      Vladislav Bukatov's website
-    </visually-hidden>
-    <div class="centered">
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img
-        class="avatar"
-        src={avatar}
-        width="150"
-        height="150"
-        alt="Vladislav Bukatov's photo" />
-      <Hand />
-    </div>
-
-    <div class="centered">
-      <h1>Vladislav Bukatov</h1>
-      <h2 class="position">frontend engineer</h2>
-    </div>
-    <block>
-      <h3 class="title">links</h3>
-      <List elements={social} />
-    </block>
-    <block>
-      <h3 class="title">skills</h3>
-      <ul>
-        {#each skills as skill}
-          <li class="skill-item">{skill}</li>
-        {/each}
-      </ul>
-    </block>
-    <block>
-      <h3 class="title">projects</h3>
-      <Projects {projects} />
-    </block>
-    <block>
-      <h3 class="title">certificates</h3>
-      <Certificates {courses} />
-    </block>
-  </div>
-</container>
